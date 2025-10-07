ccaf52fa8a1912feaf3ff8adf0063db5afd38167
Bhavesh Bhoi <bhavubhoi806@gmail.com>
2025-10-07 09:36:35 +0530

    Added Component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
+import { Login } from './login/login';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Login],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('angular-demo');
-  name = "Bhavesh Bhoi"
 }
