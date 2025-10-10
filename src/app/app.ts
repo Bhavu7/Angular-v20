2c5f09f2fb38f5fd8cc5227535fc2144ce6e5504
Bhavesh Bhoi <bhavubhoi806@gmail.com>
2025-10-10 12:05:00 +0530

    Define Datatypes and Multiple Datatypes

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,13 +5,14 @@ import { Profile } from './profile/profile.component';
 
 @Component({
   selector: 'app-root',
-  imports: [Login,Signup,Profile],
+  imports: [Login, Signup, Profile],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('angular-demo');
 
+  // calling another function from function
   btnClick() {
     alert('Button clicked!');
     this.otherFunc();
@@ -19,5 +20,30 @@ export class App {
 
   otherFunc() {
     alert('Other function called!');
+  }
+
+  // define types
+  // var_name:datatype=value
+  name: String = "Bhavesh Bhoi"
+
+  // Assign Multiple Data Types
+  age: String | number = 21
+
+  // String & Boolean
+  other: String | Boolean | Number = true
+
+  updateName() {
+    this.name = "Bhavu"
+    // this.name = 21 // error
+
+    // Assign Multiple Data Types
+    this.age = 22
+    this.age = "Bhavu"
+
+    // Boolean
+    this.other = true
+    this.other = false
+    this.other = "Bhavu"
+    this.other = 21
   }
 }
