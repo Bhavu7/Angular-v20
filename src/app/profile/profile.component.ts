0fb2dcb41695af3307d117afddbc4999ee8e3a20
Bhavesh Bhoi <bhavubhoi806@gmail.com>
2025-10-10 11:35:05 +0530

    Created new component files

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-profile',
-  templateUrl: './profile.component.html'
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.css'
 })
 
 export class Profile{
