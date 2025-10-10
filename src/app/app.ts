8ebcdc6eada4e1c5d59358d70412398c99637991
Bhavesh Bhoi <bhavubhoi806@gmail.com>
2025-10-10 11:27:33 +0530

    Created new components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { Login } from './login/login';
 import { Signup } from './signup/signup';
+import { Profile } from './profile/profile.component';
 
 @Component({
   selector: 'app-root',
-  imports: [Login,Signup],
+  imports: [Login,Signup,Profile],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
