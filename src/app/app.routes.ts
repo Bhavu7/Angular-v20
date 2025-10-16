f496c6433ae9a82fbae8d3314d6257f3b0de98e6
Bhavesh Bhoi <bhavubhoi806@gmail.com>
2025-10-16 18:23:31 +0530

    Created and Added Page Error : 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { About } from './about/about';
 import { Login } from './login/login';
 import { Contact } from './contact/contact';
 import { Home } from './home/home';
+import { PageNotFound } from './page-not-found/page-not-found';
 
 export const routes: Routes = [
   {
@@ -20,5 +21,9 @@ export const routes: Routes = [
   {
     path: 'contact',
     component: Contact
+  },
+  {
+    path: '**',
+    component: PageNotFound
   }
 ];
