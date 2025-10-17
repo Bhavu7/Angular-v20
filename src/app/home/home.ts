2a02201675e558931c440b5d616344c483c5725f
Bhavesh Bhoi <bhavubhoi806@gmail.com>
2025-10-17 13:09:32 +0530

    Created User Component

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -9,13 +9,46 @@ import { Router, RouterLink } from '@angular/router';
 })
 export class Home {
 
-  constructor(private router:Router){
+  // constructor(private router:Router){
 
-  }
+  // }
 
   // navigateToProfile(name:string){
   //   // this.router.navigate(['profile'],{queryParams:{name:'Bhavesh Bhoi'}});
   //   // this.router.navigate(['profile'],{queryParams:{name:name}});
   //   this.router.navigate(['profile'],{queryParams:{name}});
   // }
+
+  users = [
+    {
+      id:1,
+      name:'User1',
+      age: 21,
+      email: 'user@test.com'
+    },
+    {
+      id:2,
+      name:'User2',
+      age: 21,
+      email: 'user@test.com'
+    },
+    {
+      id:3,
+      name:'User3',
+      age: 21,
+      email: 'user@test.com'
+    },
+    {
+      id:4,
+      name:'User4',
+      age: 21,
+      email: 'user@test.com'
+    },
+    {
+      id:5,
+      name:'User5',
+      age: 21,
+      email: 'user@test.com'
+    }
+  ]
 }
